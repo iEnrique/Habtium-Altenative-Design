38a9591b0a474dcb88b4e9ef20e4203217d260b4
Enrique SP <esantiagop@outlook.com>
2014-10-19 18:24:10 +0200

    Update HabtiumAlternativeDesign.js

--- HabtiumAlternativeDesign.js
+++ HabtiumAlternativeDesign.js
@@ -1,18 +1,11 @@
 // ==UserScript==
 // @name         Habtium Alternative Design
-
 // @namespace    https://habtium.es/
-
 // @icon         https://habtium.es/static/images/favicon.png
-
 // @version      0.1
-
 // @description  Habtium alternative design
-
 // @author       iEnriqueSP
-
 // @match        https://habtium.es/*
-
 // @grant        none
-
+// @require      https://raw.githubusercontent.com/iEnrique/Habtium-Altenative-Design/master/general.js
 // ==/UserScript==
